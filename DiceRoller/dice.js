0e38c2a553bec57067cafa97f20c6712c28b7599
Animesh Anand <animeshanand86033@gmail.com>
2025-09-13 22:51:30 +0530

    dicejs

---
+++ DiceRoller/dice.js
@@ -0,0 +1,5 @@
+function rollDice(){
+    const numOfDice = document.getElementById("numOfDice").ariaValueMax;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = 
+}
